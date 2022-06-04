6e944b52a4e3c014386da9898c0ece118ff9d4c2
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-06-05 00:46:58 +0200

    :recycle: rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead,BufReader};
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 use anyhow::Result;
@@ -10,7 +10,8 @@ use url::Url;
 const NAME: &str = "rssg";
 const AUTHOR: &str = "github.com/lquenti";
 const VERSION: &str = "0.1";
-const ABOUT: &str = "A (nearly) zero dependency single binary static site generator for RSS/Atom feeds";
+const ABOUT: &str =
+    "A (nearly) zero dependency single binary static site generator for RSS/Atom feeds";
 
 #[derive(Parser, Debug)]
 #[clap(name=NAME, author=AUTHOR, version=VERSION,about=ABOUT,long_about=None)]
@@ -30,7 +31,8 @@ fn read_urls<P: AsRef<Path>>(filename: P) -> Result<Vec<Url>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
-    let lines: Vec<Url> = reader.lines()
+    let lines: Vec<Url> = reader
+        .lines()
         .filter_map(|l| l.ok())
         .map(|s| s.trim().to_string())
         .filter(|s| !s.is_empty())
@@ -43,14 +45,16 @@ fn read_urls<P: AsRef<Path>>(filename: P) -> Result<Vec<Url>> {
 
 // TODO make async for performance with a map to futures, which can later be joined
 // (and the logging should be part of the join)
-fn download_url(u : Url) -> Result<String> {
+fn download_url(u: Url) -> Result<String> {
     Ok(reqwest::blocking::get(u)?.text()?)
 }
 
 fn main() {
-    env_logger::init_from_env(env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"));
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
     info!("Parsing CLI");
-    let Cli {path, ..} = Cli::parse();
+    let Cli { path, .. } = Cli::parse();
 
     info!("Reading URLs");
     let urls = read_urls(path).expect("Could not read url file...");
