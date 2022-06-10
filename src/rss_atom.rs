e4a07521fd16de3ee54a2e20a836926585752d4a
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-06-10 10:52:32 +0200

    :sparkles: feeds are not joinable

--- src/rss_atom.rs
+++ src/rss_atom.rs
@@ -1,24 +1,31 @@
+use std::cmp::{self, Ordering};
 use std::str::FromStr;
 
 use crate::syndication::Feed as Synfeed;
 
+// TODO: Check if chrono is even needed
+
+pub use atom_syndication::FixedDateTime;
 use sailfish::TemplateOnce;
 
 // TODO type me correctly and implement from
-#[derive(Debug)]
+// TODO: Needs time to be sorted by relevance
+// TODO less ownership
+#[derive(Debug, Eq)]
 pub struct Entry {
     title: String,
     link: String,
     icon: String,
     source_name: String,
     source_link: String,
+    updated: FixedDateTime,
 }
 
 impl Entry {
     // TODO proper lifetimes
     fn from_atom(f: atom_syndication::Feed) -> Result<Vec<Self>, String> {
         Ok(f.entries.iter()
-            .map(|atom_syndication::Entry { title, links, .. }| {
+            .map(|atom_syndication::Entry { title, links, updated, .. }| {
                 Entry {
                     title: title.to_string(),
                     link: links.first().unwrap().href.clone(),
@@ -26,6 +33,7 @@ impl Entry {
                     icon: String::new(),
                     source_name: f.title.to_string(),
                     source_link: f.links.first().unwrap().href.clone(),
+                    updated: updated.clone(),
                 }
             }).collect()
         )
@@ -36,27 +44,63 @@ impl Entry {
     }
 }
 
+impl Ord for Entry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.updated.cmp(&other.updated)
+    }
+}
+
+impl PartialOrd for Entry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Entry {
+    fn eq(&self, other: &Self) -> bool {
+        self.updated == other.updated
+    }
+}
+
 #[derive(Debug, TemplateOnce)]
 #[template(path = "feed_template.html")]
 pub struct Feed {
     pub entries: Vec<Entry>,
-    pub updated: String, // TODO: Maybe change type?
+    pub updated: FixedDateTime,
 }
 
 impl Feed {
     fn from_atom(f: atom_syndication::Feed) -> Result<Self, String> {
         println!("{:?}", &f.links());
         Ok(Feed {
-            updated: f.updated.to_string(),
+            updated: f.updated.clone(),
             entries: Entry::from_atom(f)?,
         })
     }
 
     fn from_rss(c: rss::Channel) -> Result<Self, String> {
+        Err(String::new())
+        /*
         Ok(Feed {
             entries: Entry::from_rss(c)?,
-            updated: String::new(),
+            updated: FixedDateTime::new(),
         })
+        */
+    }
+
+    // TODO: Maybe less copies
+    pub fn join(mut self, mut other: Self)-> Self{
+        let updated = cmp::max(self.updated, other.updated);
+        // TODO merge iterators in O(n)
+        // TODO: Why the ref?
+        self.entries.append(&mut other.entries);
+        self.entries.sort();
+        Self {entries: self.entries, updated }
+    }
+
+
+    pub fn join_many(xs: &[Self]){
+        let res = xs.iter().reduce(|xs, y| xs.join(y));
     }
 }
 
